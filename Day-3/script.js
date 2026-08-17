0f152c6aa06b5345fa97a417916eae9d2eab767a
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-08-18 00:33:13 +0530

    Day 3- String Anagram Completed

--- Day-3/script.js
+++ Day-3/script.js
@@ -7,3 +7,27 @@
 //string "sakib"
 // {s:1,a:1,k:1,i:1,b:1}
 
+function isAnagarm(str1,str2){
+    if(str1.length !== str2.length){
+        return false;
+    }
+    let counter = {};
+    for(let letter of str1){
+        counter[letter] = (counter[letter] || 0) + 1;
+        console.log(counter[letter]);
+    }
+    for(let items of str2){
+        if(!counter[items]){
+            return false;
+        }
+        counter[items] -= 1;
+    }
+
+    return true;
+}
+
+const checkAnagram = isAnagarm("sakib","kisab")
+console.log(checkAnagram);
+
+
+// Linear Complexity o(n)
