24ab267a145ef8d0461ddf575ff32bad8e0401c6
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-08-23 23:55:56 +0530

    Count Even Completed

--- Day-6/script.js
+++ Day-6/script.js
@@ -42,3 +42,17 @@ function arraySum(arr){
     return sum;
 }
 console.log(arraySum([12,3,44,55,66,64,43,334,22]));
+
+//Count Even Number
+
+function countEven(arr){
+    let count=0;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] % 2 === 0){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countEven([12,3,44,55,66,64,43,334,22]));
