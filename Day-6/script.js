7a9debc285eb951cc2a5c33e04447a77dfd2cc1d
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-08-23 23:41:15 +0530

    Find max Completed

--- Day-6/script.js
+++ Day-6/script.js
@@ -1,1 +1,19 @@
-// Day-6
+// Array Question 
+//find Max
+//Find Min
+//find Sum
+//Find Even
+// Array Reverse
+
+//Find Max
+
+function findMax(arr){
+    let max=[];
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] > max){
+            max=arr[i];
+        }
+    }
+    return max;
+}
+console.log(findMax([12,3,44,55,66,64,43,334,,22]));
