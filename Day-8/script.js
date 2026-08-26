f8b3b3d0a2cfb984fb0366a6c064e4322bd7dc2e
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-08-26 22:55:47 +0530

    Day 8- Bubble Sort Completed

--- Day-8/script.js
+++ Day-8/script.js
@@ -1,1 +1,43 @@
 // Bubble Sort
+
+function bubbleSort(arr){
+    for(let i=0; i<arr.length; i++){
+
+        for(let j=0; j<arr.length - i -1; j++){
+
+            if( arr[j] > arr[j +1]){
+
+                let temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    return arr;
+}
+console.log(bubbleSort([6,23,4,55,21,2]));
+
+
+
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let swapped = false; // Track if any swap occurs in this pass
+
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                
+                swapped = true; // Mark that a swap took place
+            }
+        }
+
+        // If no two elements were swapped in the inner loop, array is sorted
+        if (!swapped) break;
+    }
+    return arr;
+}
+
+console.log(bubbleSort([10,20,30,15])); 
