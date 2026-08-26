bda04a5cce41620782aa42242de875d0d8811e65
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-08-26 23:07:04 +0530

    Bubble and Selection Sort Completed

--- Day-8/script.js
+++ Day-8/script.js
@@ -43,3 +43,19 @@ function bubbleSort(arr) {
 console.log(bubbleSort([10,20,30,15])); 
 
 //Selection Sort
+
+function selectionSort(arr){
+    for(let i=0; i<arr.length; i++){
+        let minIndex=i;
+        for(let j=i+1; j<arr.length; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex=j;
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    return arr;
+}
+console.log(selectionSort([5, 3, 8, 1, 2]));
