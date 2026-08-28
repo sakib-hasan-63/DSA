356ba07ef6ba9af47d9d4eb6648694ffd1bfc9b5
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-08-28 23:13:06 +0530

    Reverse String Completed

--- Day-10/script.js
+++ Day-10/script.js
@@ -7,3 +7,14 @@ let str = "sakib";
 for(let i=0; i<str.length; i++){
     console.log(str[i]);
 }
+
+// Reverse String 
+
+function reverseString(str){
+    let result = "";
+    for(let i= str.length-1; i>=0; i--){
+        result += str[i];
+    }
+    return result;
+}
+console.log(reverseString("sakib"));
