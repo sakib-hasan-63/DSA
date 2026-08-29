6cba9443886c78afe062105f5d3e958ccc76e9e5
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-08-29 13:27:41 +0530

    String Anagram Completed

--- Day-11/script.js
+++ Day-11/script.js
@@ -1,1 +1,21 @@
 // String Anagram
+
+function isAnagram(str1,str2){
+    if(str1.length !== str2.length){
+        return false;
+    }
+    let count = {};
+    for(let char of str1){
+        count[char] = (count[char] || 0 ) + 1;
+    }
+    for(let char of str2){
+        if(!count[char]){
+            return false;
+        }
+        count[char]--;
+    }
+    return true;
+}
+
+console.log(isAnagram("listen", "silent"));
+console.log(isAnagram("listen", "silenntt"));
