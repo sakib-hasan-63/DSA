d2d826e851a7e704e4726f7f8c2b4eef481a6626
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-08-31 18:03:53 +0530

    Fibonacci Recursive Completed

--- Day-13/script.js
+++ Day-13/script.js
@@ -1,1 +1,13 @@
-console.log(" Day 13");
+// Fibonacci Recursive
+
+function fibonacci(num){
+    if(num === 0){
+        return 0;
+    }
+    if(num === 1){
+        return 1;
+    }
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+console.log(fibonacci(6));
