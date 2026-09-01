09759d842258bcdd2533851817911c219a99cef4
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-09-01 23:20:04 +0530

    Day-14

--- Day-14/script.js
+++ Day-14/script.js
@@ -1,1 +1,36 @@
-console.log("day 14");
+//Binary search Recurive 
+
+function binarySearchRecursive(arr, target, left, right) {
+
+    if (left > right) {
+        return -1;
+    }
+
+    const mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+        return mid;
+    }
+
+    if (arr[mid] < target) {
+        return binarySearchRecursive(
+            arr,
+            target,
+            mid + 1,
+            right
+        );
+    }
+
+    return binarySearchRecursive(
+        arr,
+        target,
+        left,
+        mid - 1
+    );
+}
+
+const arr = [10, 20, 30, 40, 50, 60, 70];
+
+console.log(
+    binarySearchRecursive(arr, 50, 0, arr.length - 1)
+);
